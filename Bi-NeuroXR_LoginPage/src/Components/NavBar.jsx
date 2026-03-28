c76296bae8fcfeefa2c90e3b118fb7845ffa41d9
TumiXR <tumelokola52@gmail.com>
2026-03-28 22:23:10 +0200

    brand profile and styling

--- Bi-NeuroXR_LoginPage/src/Components/NavBar.jsx
+++ Bi-NeuroXR_LoginPage/src/Components/NavBar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import logo_image from '/src/assets/bi-neuroxr-logo.svg'
 
 const Loginpage = () => {
   return (
     
       <header>
 
-        <h2 className='logo'>Bi-NeuroXR</h2>
-        <nav className='navigation'>
-          <a href='#'>Home</a>
-          <a href='#'>About</a>
-          <a href='#'>Service</a>
-          <a href='#'>Support</a>
-          <button className='btnLogin-popup'>Login</button>
-        </nav>
+          <div className='brand'>
+             <img className = "logo_image" src={logo_image} alt= "img logo" />
+             <h2 className='logo'>Bi-NeuroXR</h2>
+          </div>
+
+          <nav className='navigation'>
+            <a href='#'>Home</a>
+              <a href='#'>About</a>
+                <a href='#'>Service</a>
+              <a href='#'>Support</a>
+            <button className='btnLogin-popup'>Login</button>
+          </nav>
 
       </header>
   )     
